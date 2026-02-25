d5142187d664a5925c2673203b8d6246ac2bb247
Andriy Sak <andreyysak17@gmail.com>
2026-02-25 15:51:34 +0200

    feat: connect all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import {ConfigModule, ConfigService} from "@nestjs/config";
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
-import {AuthModule} from "./modules/auth/auth.module";
+import { AuthModule } from './modules/auth/auth.module';
 import { TripModule } from './modules/trip/trip.module';
 import { CarModule } from './modules/car/car.module';
 import { FuelModule } from './modules/fuel/fuel.module';
@@ -15,47 +15,47 @@ import { TransactionModule } from './modules/transaction/transaction.module';
 import { MaintenanceModule } from './modules/maintenance/maintenance.module';
 import { MoviesModule } from './modules/movies/movies.module';
 import { SeriesModule } from './modules/series/series.module';
-import {WorkoutsModule} from "./modules/workout/workout.module";
+import { WorkoutsModule } from './modules/workout/workout.module';
 import { MonobankModule } from './modules/monobank/monobank.module';
 import { WeatherModule } from './modules/weather/weather.module';
 import { ExportModule } from './export/export.module';
 
 @Module({
   imports: [
-      ConfigModule.forRoot({
-          isGlobal: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_DB_HOST'),
+        port: configService.get<number>('POSTGRES_DB_PORT'),
+        username: configService.get<string>('POSTGRES_DB_USERNAME'),
+        password: configService.get<string>('POSTGRES_DB_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
       }),
-      TypeOrmModule.forRootAsync({
-          imports: [ConfigModule],
-          inject: [ConfigService],
-          useFactory: (configService: ConfigService) => ({
-              type: 'postgres',
-              host: configService.get<string>('POSTGRES_DB_HOST'),
-              port: configService.get<number>('POSTGRES_DB_PORT'),
-              username: configService.get<string>('POSTGRES_DB_USERNAME'),
-              password: configService.get<string>('POSTGRES_DB_PASSWORD'),
-              database: configService.get<string>('POSTGRES_DB_NAME'),
-              autoLoadEntities: true,
-              synchronize: true,
-              logging: true,
-          }),
-      }),
-      UserModule,
-      AuthModule,
-      TripModule,
-      CarModule,
-      FuelModule,
-      AnalysisModule,
-      AccountModule,
-      CategoryModule,
-      TransactionModule,
-      MaintenanceModule,
-      MoviesModule,
-      SeriesModule,
-      WorkoutsModule,
-      MonobankModule,
-      WeatherModule,
-      ExportModule
+    }),
+    UserModule,
+    AuthModule,
+    TripModule,
+    CarModule,
+    FuelModule,
+    AnalysisModule,
+    AccountModule,
+    CategoryModule,
+    TransactionModule,
+    MaintenanceModule,
+    MoviesModule,
+    SeriesModule,
+    WorkoutsModule,
+    MonobankModule,
+    WeatherModule,
+    ExportModule,
   ],
   controllers: [AppController],
   providers: [AppService],
