d21935aa388bcb8d31f76b71759eb1abe2418c78
Andriy Sak <andreyysak17@gmail.com>
2026-03-02 12:07:05 +0200

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5187',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   })
